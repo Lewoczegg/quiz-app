b61b576230b974994b9b76adf4150d04e34c1761
Lewoczegg <mateusz.lewocz46@gmail.com>
2024-01-12 22:44:45 +0100

    Add user repository

---
+++ backend/src/main/java/lewocz/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package lewocz.backend.repository;
+
+import lewocz.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
