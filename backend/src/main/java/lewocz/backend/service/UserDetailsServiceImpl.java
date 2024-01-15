4c29c0b82959d60174d089a1eb3dbbeeaff669cc
Lewoczegg <mateusz.lewocz46@gmail.com>
2024-01-15 20:15:19 +0100

    Add userDetailsServiceImpl

---
+++ backend/src/main/java/lewocz/backend/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package lewocz.backend.service;
+
+import lewocz.backend.exception.NotFoundException;
+import lewocz.backend.model.User;
+import lewocz.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() ->
+                new NotFoundException(String.format("User does not exist, email: %s", email)));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+}
