6bedc1ec80fb6ed6b1963d9d6419110ce288a481
Mateusz Lewocz <82779@student.pb.edu.pl>
2024-02-19 20:56:54 +0100

    Improve quizOption UI

--- quiz-app-frontend/src/components/QuizOption.tsx
+++ quiz-app-frontend/src/components/QuizOption.tsx
@@ -14,13 +14,18 @@ const QuizOption = ({ answer, isSelected, onChange, showCorrect }: Props) => {
         answer.isCorrect && showCorrect ? "bg-green-200 rounded-lg" : ""
       }`}
     >
-      <label className="flex items-center">
+      <label
+        className={`flex items-center ${!showCorrect ? "cursor-pointer" : ""}`}
+      >
         <input
           type="radio"
           name="answer"
           checked={isSelected}
           onChange={onChange}
-          className="mr-2 appearance-none w-4 h-4 border border-primary-blue rounded-full checked:bg-primary-blue checked:border-transparent focus:outline-none shrink-0"
+          className={`mr-2 appearance-none w-4 h-4 border border-primary-blue rounded-full checked:bg-primary-blue checked:border-transparent
+           focus:outline-none shrink-0 ${
+             !showCorrect ? "cursor-pointer disabled" : ""
+           }`}
         />
         <span
           className={`text-lg ${
